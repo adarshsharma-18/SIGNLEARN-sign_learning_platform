742ef2cc7e0c525e0c89a3728a296cb918203698
adarshsharma-18 <141864155+adarshsharma-18@users.noreply.github.com>
2025-03-09 01:06:11 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "/SIGNLEARN-sign_learning_platform"
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
